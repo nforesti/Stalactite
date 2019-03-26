7bda776c3a6942b4c0fab7fa1d51d0dfc9e0f59d
Neve Foresti <nforesti@ucsd.edu>
2019-03-26 12:31:30 -0700

    finished the README and dataplot.java

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Stalactite
-My first attempt at data visualization for Vis 41
+My first attempt at data visualization for Vis 41 (Design Communication). My project theme was related to isolation, so I searched
+datasets on Kaggle. I wanted to see what would happen when I plotted the data of a suicide dataset. It made a very
+interesting image, so I played around more with the styling.
+
+## Note on the datasource
+I got the data from Kaggle (https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016). In newdataplot.csv,
+I only got the US data and from 1993 to 2015. I then isolated the columns: year, age, suicides_no, and population. Sorting
+the data also does some really cool things to the image. I also changed the values of the age group to the average of the range.
+newdataplot.csv is the dataset I've used. originalUS.xlsx is the original data with all of the columns (only the US data).
+
+
+### Example
+
+<img src="https://drive.google.com/uc?export=view&id=1mqPdhpBSs-kqNoatX2dRc872sKeKUhxz" alt="large circles with purple outline. black and white fill" width="300"/>
+<img src="https://drive.google.com/uc?export=view&id=1UOLYXOSLzXyveYI63zTCOQnW1wqjrTiE" alt="large squares with purple outline. black and white fill" width="300"/>
+<img src="https://drive.google.com/uc?export=view&id=1ZaHPWG-18NZN0pTdQ9JubsS7abD5nr9k" alt="large circles with purple outline. black and white fill" width="300"/>
+
+
+## Author
+
+* **Neve Foresti** - (https://github.com/nforesti)
+
+## Acknowledgments
+Data Source: https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
--- dataplot.java
+++ dataplot.java
@@ -1,3 +1,10 @@
+/**
+ * Filename: dataplot.java
+ * Author: Neve Foresti
+ * Email: neve.foresti@gmail.com
+ * Description: Contains dataplot class, which using JavaFX and css, plots values in CSV file
+ * and creates cool 3-D image
+ */
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.NumberAxis;
@@ -6,52 +13,55 @@ import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import java.util.Set;
 import javafx.scene.paint.Color;
-
 import javafx.scene.Node;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class dataplot extends Application {
 
-  @Override public void start(Stage stage) {
-    stage.setTitle("Scatter Chart Sample");
-    final NumberAxis xAxis = new NumberAxis(1975, 2020, 45);
+  @Override
+  public void start(Stage stage) {
+
+    stage.setTitle("Stalactite");
+    stage.setWidth(1500);
+    stage.setHeight(1000);
+
+    final NumberAxis xAxis = new NumberAxis(1990, 2020, 45);
     final NumberAxis yAxis = new NumberAxis(0, 12000, 12000);
-    final ScatterChart<Number,Number> sc = new
-    ScatterChart<Number,Number>(xAxis,yAxis);
+    xAxis.setTickLabelsVisible(false);
+    yAxis.setTickLabelsVisible(false);
+    final ScatterChart<Number, Number> sc = new ScatterChart<Number, Number>(xAxis, yAxis);
 
     XYChart.Series series1 = new XYChart.Series();
 
-
-
-
     BufferedReader reader;
-    int[][] data = new int[430][2];
+    int[][] data = new int[300 /*input any value, as long as greater than num of csv rows*/][2];
     try {
-      reader = new BufferedReader(new FileReader(
-      "C:\\Users\\nevef\\Documents\\Portfolio\\newdataplot.csv"));
+      reader = new BufferedReader(new FileReader("newdataplot.csv")); /*data file*/
 
-      String line = reader.readLine();
+      String line = reader.readLine(); // first line of data. will not plot this
+
+      // variables associated with columns in data
       int year;
       int age;
-      int counter = 0;
       int suicides;
       int population;
 
-      String rest = "";
-      while ((line = reader.readLine())!=null) {
-        String splitted[] =line.split(",",2);
+      String rest = ""; //used for substring later in function
+      int counter = 0;
+
+      while ((line = reader.readLine()) != null) {
+        //get year
+        String splitted[] = line.split(",", 2); 
         year = Integer.parseInt(splitted[0]);
         rest = splitted[1];
-
-        splitted=rest.split(",",2);
+        //get age
+        splitted = rest.split(",", 2);
         age = Integer.parseInt(splitted[0]);
         rest = splitted[1];
-
-        splitted =rest.split(",",2);
+        //get suicides and population
+        splitted = rest.split(",", 2);
         suicides = Integer.parseInt(splitted[0]);
         rest = splitted[1];
         population = Integer.parseInt(rest.substring(0));
@@ -59,48 +69,45 @@ public class dataplot extends Application {
         data[counter][0] = population;
         data[counter][1] = age;
 
+        //add data to series
         series1.getData().add(new XYChart.Data(year, suicides));
         counter++;
       }
-      System.out.println("out");
       reader.close();
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-
-
     sc.getData().addAll(series1);
-    Scene scene  = new Scene(sc, 800, 600, Color.BLACK);
+    
+    Scene scene = new Scene(sc, 800, 600, Color.BLACK);
+    scene.getStylesheets().add("style.css"); //hide axis information
 
     stage.setScene(scene);
     stage.show();
 
+    //go through each datapoint, change color and radius
     Set<Node> nodes = sc.lookupAll(".series" + 0);
     int counter = 0;
     for (Node n : nodes) {
-      if (counter < 430){
-        System.out.println(counter);
-        int radius = data[counter][0];
-        String color = "";
-        if (counter % 2 == 0){
-          color = "white";
-        }
-        else{
-          color = "black";
-        }
-        System.out.println("radius " + radius);
-        n.setStyle("-fx-background-color: #860061, "+color+";\n"
-        + "    -fx-background-insets: 0, 2;\n"
-        + "    -fx-background-radius: " + radius + "px;\n"
-        + "    -fx-padding: " + radius * .6+ "px;");
+      int radius = data[counter][0]; //radius coiincides with population size
+
+      /*rotate b/w black and white fill*/
+      String color = "";
+      if (counter % 2 == 0) {
+        color = "white";
+      } else {
+        color = "black";
       }
+
+      //change fill color and radius
+      n.setStyle("-fx-background-color: #860061, " + color + ";\n" + "    -fx-background-insets: 0, 2;\n"
+          + "    -fx-background-radius: " + radius + "px;\n" + "    -fx-padding: " + (radius + 15) + "px;");
       counter++;
     }
   }
 
   public static void main(String[] args) {
     launch(args);
-
   }
 }
